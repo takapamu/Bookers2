d98091bcac860bc5e5ee63a0f3419d0cd74cd198
takapamui <takapamukyary0129@gmail.com>
2021-04-25 09:24:45 +0000

    [Finish]4-10

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -38,8 +38,8 @@ class BooksController < ApplicationController
   end
 
   def update
-    @book = Book.find(params[:id])
-    @book.user_id = current_user.id
+      @book = Book.find(params[:id])
+      @book.user_id = current_user.id
     if @book.update(book_params)
       flash[:notice]="Book was successfully updated."
       redirect_to book_path(@book.id)
